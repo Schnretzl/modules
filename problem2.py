56e060c81a8e3dbcc2de774e64159900162b754b
Schnretzl <wrhendley@gmail.com>
2024-09-16 15:34:45 -0500

    Add problem 2 and text_utils

---
+++ problem2.py
@@ -0,0 +1,7 @@
+# 2. Mastering Python Modules and Aliases 
+
+# Task 1: Custom Module with Aliases
+import text_utils as tu
+s = input("Enter a string: ")
+print(f"Reversed: {tu.reverse_string(s)}")
+print(f"Capitalized: {tu.capitalize_string(s)}")
